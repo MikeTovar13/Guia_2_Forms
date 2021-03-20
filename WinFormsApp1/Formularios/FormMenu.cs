d91e795b248ab962f607d5ff42a32a962e5824de
Daniela <danielaespinosa51@gmail.com>
2021-03-20 09:16:42 -0500

    Comiit initial

---
+++ WinFormsApp1/Formularios/FormMenu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinFormsApp1.Formularios {
+    public partial class FormMenu : Form {
+        public FormMenu() {
+            InitializeComponent();
+        }
+    }
+}
