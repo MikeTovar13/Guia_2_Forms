dd7e9acee51d567169be5f447579008b0bd59be4
Daniela <danielaespinosa51@gmail.com>
2021-03-22 22:43:15 -0500

    -programa de nómina terminado

--- WinFormsApp1/Formularios/Form10.Designer.cs
+++ WinFormsApp1/Formularios/Form10.Designer.cs
@@ -29,7 +29,28 @@ namespace WinFormsApp1.Formularios {
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.txt_cc = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_dias = new System.Windows.Forms.TextBox();
+            this.txt_salario = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txt_heNocturnas = new System.Windows.Forms.TextBox();
+            this.txt_heDiurnas = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txt_hRecargoNocturno = new System.Windows.Forms.TextBox();
+            this.txt_heFestivas = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.btn_calcular = new System.Windows.Forms.Button();
+            this.dgvNomina = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNomina)).BeginInit();
             this.SuspendLayout();
             // 
             // btn_salir
@@ -84,11 +105,218 @@ namespace WinFormsApp1.Formularios {
             this.pictureBox1.TabIndex = 10;
             this.pictureBox1.TabStop = false;
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(12, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(241, 21);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Calcular nómina de empeados";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(12, 98);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(582, 15);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "Llena los datos y cada vez que le des al botón \"Agregar\" calculará la nómina y ag" +
+    "regará el empleado a la tabla";
+            // 
+            // txt_name
+            // 
+            this.txt_name.Location = new System.Drawing.Point(151, 159);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(100, 23);
+            this.txt_name.TabIndex = 31;
+            // 
+            // txt_cc
+            // 
+            this.txt_cc.Location = new System.Drawing.Point(151, 126);
+            this.txt_cc.Name = "txt_cc";
+            this.txt_cc.Size = new System.Drawing.Size(100, 23);
+            this.txt_cc.TabIndex = 30;
+            this.txt_cc.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_cc_KeyPress);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(19, 162);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(126, 15);
+            this.label7.TabIndex = 29;
+            this.label7.Text = "Nombre del empleado";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(17, 130);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(119, 15);
+            this.label6.TabIndex = 28;
+            this.label6.Text = "Cédula del empleado";
+            // 
+            // txt_dias
+            // 
+            this.txt_dias.Location = new System.Drawing.Point(350, 159);
+            this.txt_dias.Name = "txt_dias";
+            this.txt_dias.Size = new System.Drawing.Size(100, 23);
+            this.txt_dias.TabIndex = 35;
+            this.txt_dias.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_dias_KeyPress);
+            // 
+            // txt_salario
+            // 
+            this.txt_salario.Location = new System.Drawing.Point(350, 127);
+            this.txt_salario.Name = "txt_salario";
+            this.txt_salario.Size = new System.Drawing.Size(100, 23);
+            this.txt_salario.TabIndex = 34;
+            this.txt_salario.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_salario_KeyPress);
+            this.txt_salario.Leave += new System.EventHandler(this.txt_salario_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(257, 162);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(87, 15);
+            this.label8.TabIndex = 33;
+            this.label8.Text = "Días trabajados";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(257, 130);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(79, 15);
+            this.label9.TabIndex = 32;
+            this.label9.Text = "Salario (Valor)";
+            // 
+            // txt_heNocturnas
+            // 
+            this.txt_heNocturnas.Location = new System.Drawing.Point(568, 159);
+            this.txt_heNocturnas.Name = "txt_heNocturnas";
+            this.txt_heNocturnas.Size = new System.Drawing.Size(39, 23);
+            this.txt_heNocturnas.TabIndex = 39;
+            this.txt_heNocturnas.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_heNocturnas_KeyPress);
+            // 
+            // txt_heDiurnas
+            // 
+            this.txt_heDiurnas.Location = new System.Drawing.Point(568, 127);
+            this.txt_heDiurnas.Name = "txt_heDiurnas";
+            this.txt_heDiurnas.Size = new System.Drawing.Size(39, 23);
+            this.txt_heDiurnas.TabIndex = 38;
+            this.txt_heDiurnas.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_heDiurnas_KeyPress);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label10.Location = new System.Drawing.Point(456, 162);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(106, 15);
+            this.label10.TabIndex = 37;
+            this.label10.Text = "H extras nocturnas";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label11.Location = new System.Drawing.Point(458, 130);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(92, 15);
+            this.label11.TabIndex = 36;
+            this.label11.Text = "H extras diurnas";
+            // 
+            // txt_hRecargoNocturno
+            // 
+            this.txt_hRecargoNocturno.Location = new System.Drawing.Point(721, 159);
+            this.txt_hRecargoNocturno.Name = "txt_hRecargoNocturno";
+            this.txt_hRecargoNocturno.Size = new System.Drawing.Size(39, 23);
+            this.txt_hRecargoNocturno.TabIndex = 43;
+            this.txt_hRecargoNocturno.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_hRecargoNocturno_KeyPress);
+            // 
+            // txt_heFestivas
+            // 
+            this.txt_heFestivas.Location = new System.Drawing.Point(721, 127);
+            this.txt_heFestivas.Name = "txt_heFestivas";
+            this.txt_heFestivas.Size = new System.Drawing.Size(39, 23);
+            this.txt_heFestivas.TabIndex = 42;
+            this.txt_heFestivas.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_heFestivas_KeyPress);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label12.Location = new System.Drawing.Point(611, 162);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(104, 15);
+            this.label12.TabIndex = 41;
+            this.label12.Text = "H recargo nocturo";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label13.Location = new System.Drawing.Point(613, 130);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(92, 15);
+            this.label13.TabIndex = 40;
+            this.label13.Text = "H extras festivas";
+            // 
+            // btn_calcular
+            // 
+            this.btn_calcular.Location = new System.Drawing.Point(713, 211);
+            this.btn_calcular.Name = "btn_calcular";
+            this.btn_calcular.Size = new System.Drawing.Size(75, 23);
+            this.btn_calcular.TabIndex = 60;
+            this.btn_calcular.Text = "Agregar";
+            this.btn_calcular.UseVisualStyleBackColor = true;
+            this.btn_calcular.Click += new System.EventHandler(this.btn_calcular_Click);
+            // 
+            // dgvNomina
+            // 
+            this.dgvNomina.AllowUserToAddRows = false;
+            this.dgvNomina.AllowUserToDeleteRows = false;
+            this.dgvNomina.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvNomina.Location = new System.Drawing.Point(12, 200);
+            this.dgvNomina.Name = "dgvNomina";
+            this.dgvNomina.ReadOnly = true;
+            this.dgvNomina.RowTemplate.Height = 25;
+            this.dgvNomina.Size = new System.Drawing.Size(693, 195);
+            this.dgvNomina.TabIndex = 61;
+            // 
             // Form10
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvNomina);
+            this.Controls.Add(this.btn_calcular);
+            this.Controls.Add(this.txt_hRecargoNocturno);
+            this.Controls.Add(this.txt_heFestivas);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.txt_heNocturnas);
+            this.Controls.Add(this.txt_heDiurnas);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txt_dias);
+            this.Controls.Add(this.txt_salario);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txt_name);
+            this.Controls.Add(this.txt_cc);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -98,6 +326,7 @@ namespace WinFormsApp1.Formularios {
             this.Text = "10. Nómina de empleados ";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form10_FormClosed);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNomina)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -110,5 +339,25 @@ namespace WinFormsApp1.Formularios {
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.TextBox txt_cc;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_dias;
+        private System.Windows.Forms.TextBox txt_salario;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txt_heNocturnas;
+        private System.Windows.Forms.TextBox txt_heDiurnas;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txt_hRecargoNocturno;
+        private System.Windows.Forms.TextBox txt_heFestivas;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Button btn_calcular;
+        private System.Windows.Forms.DataGridView dgvNomina;
     }
 }
--- WinFormsApp1/Formularios/Form10.cs
+++ WinFormsApp1/Formularios/Form10.cs
@@ -8,11 +8,33 @@ using System.Windows.Forms;
 
 namespace WinFormsApp1.Formularios {
     public partial class Form10 : Form {
-
+        DataTable tabla = new DataTable();
         Form menu;
+        double salarioBasico = 908526; // Salario basico
+
         public Form10(Form menu) {
             this.menu = menu;
             InitializeComponent();
+            dgvNomina.DataSource = tabla;
+            tabla.Columns.Add("Cédula", typeof(string));
+            tabla.Columns.Add("Nombre", typeof(string));
+            tabla.Columns.Add("Salario", typeof(string));
+            tabla.Columns.Add("Aux. de transporte", typeof(string));
+            tabla.Columns.Add("Días trabajados", typeof(int));
+            tabla.Columns.Add("Sueldo", typeof(string));
+            tabla.Columns.Add("Horas diurnas", typeof(string));
+            tabla.Columns.Add("Horas nocturas", typeof(string));
+            tabla.Columns.Add("Horas festivas", typeof(string));
+            tabla.Columns.Add("Recargo nocturno", typeof(string));
+            tabla.Columns.Add("Total Devengado", typeof(string));
+            tabla.Columns.Add("Salud", typeof(string));
+            tabla.Columns.Add("Pensión", typeof(string));
+            tabla.Columns.Add("Fondo de solidaridad", typeof(string));
+            tabla.Columns.Add("Total deducciones", typeof(string));
+            tabla.Columns.Add("Neto pagado", typeof(string));
+            dgvNomina.ColumnHeadersDefaultCellStyle.Font = new Font(dgvNomina.ColumnHeadersDefaultCellStyle.Font, FontStyle.Bold);
+            dgvNomina.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
         }
 
         private void Form10_FormClosed(object sender, FormClosedEventArgs e) {
@@ -21,6 +43,128 @@ namespace WinFormsApp1.Formularios {
 
         private void btn_salir_Click(object sender, EventArgs e) {
             this.Close();
+        }
+
+        private void btn_calcular_Click(object sender, EventArgs e) {
+
+            double[] datos_in = new double[7];
+            double[] datos_out = new double[13];
+
+            if (String.IsNullOrEmpty(txt_cc.Text)) { MessageBox.Show("El campo cédula es obligatorio", "Campo Vacio"); return; }
+            if (String.IsNullOrEmpty(txt_name.Text)){ MessageBox.Show("El campo nombre es obligatorio", "Campo Vacio"); return; }
+            if (String.IsNullOrEmpty(txt_salario.Text)) { MessageBox.Show("El campo salario es obligatorio", "Campo Vacio"); return; }
+            if (String.IsNullOrEmpty(txt_dias.Text)) { MessageBox.Show("El campo días trabajados es obligatorio", "Campo Vacio"); return; }
+            if (int.Parse(txt_dias.Text)>30 || int.Parse(txt_dias.Text) == 0) { MessageBox.Show("El campo días trabajados debe ser un valor entre 1 y 30", "Campo Vacio"); return; }
+
+            datos_in[0] = double.Parse(txt_salario.Text);
+            datos_in[1] = double.Parse(txt_dias.Text) ;
+            if (String.IsNullOrEmpty(txt_heDiurnas.Text)) datos_in[2] = 0;
+            else datos_in[2] = double.Parse(txt_heDiurnas.Text);
+            if (String.IsNullOrEmpty(txt_heNocturnas.Text)) datos_in[3] = 0;
+            else datos_in[3] = double.Parse(txt_heNocturnas.Text);
+            if (String.IsNullOrEmpty(txt_heFestivas.Text)) datos_in[4] = 0;
+            else datos_in[4] = double.Parse(txt_heFestivas.Text);
+            if (String.IsNullOrEmpty(txt_hRecargoNocturno.Text)) datos_in[5] = 0;
+            else datos_in[5] = double.Parse(txt_hRecargoNocturno.Text);
+
+            datos_out = this.calcularNomina(datos_in);
+
+            tabla.Rows.Add(
+                txt_cc.Text, txt_name.Text,
+                datos_out[0].ToString("C"), datos_out[1].ToString("C"),
+                datos_out[2], datos_out[3].ToString("C"),
+                datos_out[4].ToString("C"), datos_out[5].ToString("C"),
+                datos_out[6].ToString("C"), datos_out[7].ToString("C"),
+                datos_out[8].ToString("C"), datos_out[9].ToString("C"),
+                datos_out[10].ToString("C"), datos_out[11].ToString("C"),
+                datos_out[12].ToString("C"), datos_out[13].ToString("C"));
+
+            // Datos básicos
+            txt_cc.Text =""; // Cedula
+            txt_name.Text=""; // Nombre empleado
+            txt_salario.Text=""; // Nombre empleado
+            txt_dias.Text=""; // Nombre empleado
+            txt_heDiurnas.Text=""; // Nombre empleado
+            txt_heFestivas.Text= ""; // Nombre empleado
+            txt_hRecargoNocturno.Text=""; // Nombre empleado
+            txt_heNocturnas.Text = ""; // Nombre empleado
+
+
+            dgvNomina.Refresh();
+
+        }
+
+        private double[] calcularNomina(double[] info_emp) {
+
+            // Calculo basico
+            double salarioDia = info_emp[0] / 30;
+            double valorHora = salarioDia / 8;
+            double sueldo = salarioDia * info_emp[1];
+            double auxilioTransporte = 0;
+            double fondoSolidaridad = 0;
+
+            // Calculos extras
+            double hExtraDiurna = ((valorHora * 125) / 100) * (double)info_emp[2]; // cobro al 1,25%
+            double hExtraNocturna = ((valorHora * 175) / 100) * (double)info_emp[3]; // cobro al 1,75%
+            double hExtraFestiva = ((valorHora * 175) / 100) * (double)info_emp[4]; // cobro al 1,75%;
+            double recargoNocturno = ((valorHora * 35) / 100) * (double)info_emp[5];
+
+            if ((double)info_emp[0] <= salarioBasico * 2) {
+                auxilioTransporte = 106454;
+            }
+
+            if (sueldo >= salarioBasico * 4) {
+                fondoSolidaridad = (sueldo * 1) / 100;
+            }
+
+            double horasExtras = hExtraFestiva + hExtraDiurna + hExtraNocturna + recargoNocturno;
+
+            double total_1 = sueldo + horasExtras; // Sueldo total
+
+            double salud = (total_1 * 4) / 100;
+            double pension = (total_1 * 4) / 100;
+
+            double deducciones = fondoSolidaridad + salud + pension;
+
+            double total_2 = total_1 - deducciones + auxilioTransporte; // Sueldo con deducciones basicas
+
+            // Sueldo total con deducciones y extras calculados
+            return new double[]{info_emp[0], auxilioTransporte, info_emp[1], sueldo, hExtraDiurna, hExtraNocturna,
+                hExtraFestiva, recargoNocturno, total_1, salud, pension, fondoSolidaridad, deducciones, total_2 };
+
+        }
+
+        private void txt_cc_KeyPress(object sender, KeyPressEventArgs e) {
+            e.Handled = Utils.validarInt(e.KeyChar);
+        }
+
+        private void txt_salario_KeyPress(object sender, KeyPressEventArgs e) {
+            e.Handled = Utils.validarInt(e.KeyChar);
+        }
+
+        private void txt_dias_KeyPress(object sender, KeyPressEventArgs e) {
+            e.Handled = Utils.validarInt(e.KeyChar);
+        }
+
+        private void txt_heDiurnas_KeyPress(object sender, KeyPressEventArgs e) {
+            e.Handled = Utils.validarInt(e.KeyChar);
+        }
+
+        private void txt_heNocturnas_KeyPress(object sender, KeyPressEventArgs e) {
+            e.Handled = Utils.validarInt(e.KeyChar);
+        }
+
+        private void txt_heFestivas_KeyPress(object sender, KeyPressEventArgs e) {
+            e.Handled = Utils.validarInt(e.KeyChar);
+        }
+
+        private void txt_hRecargoNocturno_KeyPress(object sender, KeyPressEventArgs e) {
+            e.Handled = Utils.validarInt(e.KeyChar);
+        }
+
+        private void txt_salario_TextChanged(object sender, EventArgs e) {
+            double sl = Convert.ToDouble(txt_salario.Text);
+            txt_salario.Text = sl.ToString("#,#");
         }
     }
 }
