1f44cc12faef5f3ec453265bfbd062a6d6482aae
Michael Tovar <michaelf.tovart@ecci.edu.co>
2021-04-23 18:35:21 -0500

    Ajuste final para nomina que se pueda ver la tabla

--- WinFormsApp1/Formularios/Form10.Designer.cs
+++ WinFormsApp1/Formularios/Form10.Designer.cs
@@ -227,7 +227,6 @@ namespace WinFormsApp1.Formularios {
             this.dgvNomina.AllowUserToAddRows = false;
             this.dgvNomina.AllowUserToDeleteRows = false;
             this.dgvNomina.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvNomina.Enabled = false;
             this.dgvNomina.Location = new System.Drawing.Point(12, 140);
             this.dgvNomina.Name = "dgvNomina";
             this.dgvNomina.ReadOnly = true;
